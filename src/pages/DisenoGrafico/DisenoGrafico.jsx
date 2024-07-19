14d8bb9b5e960fc8c438a6da4e09c22a7aa0e318
Ajandro <adelaygleisafleitas@gmail.com>
2024-07-19 15:11:27 -0400

    3

--- src/pages/DisenoGrafico/DisenoGrafico.jsx
+++ src/pages/DisenoGrafico/DisenoGrafico.jsx
@@ -1,8 +1,5 @@
 import { useEffect, useState } from "react";
-import {
-  disenoGrafico,
-  baner,
-} from "../../components/data/pages/disenoGrafico.js";
+import {  disenoGrafico,  baner} from "../../components/data/pages/disenoGrafico.js";
 import Gallery from "../tools/Gallery/Gallery.jsx";
 import "./DisenoGrafico.css";
 import LazyLoad from "react-lazy-load";
