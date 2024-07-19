84bb23e6dec431e5007f5a49e355dbef27e63af5
Ajandro <adelaygleisafleitas@gmail.com>
2024-07-19 15:07:42 -0400

    2

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import "./App.css";
 import "./components/footer/Footer.css";
 import "./components/Header/Header.css";
-import {  Header,  Main,  Footer,  Films,  Fotografia,  Publicidad,  MotionGraphic,  DisenoGrafico,  Proyectos3d,} from "./App.js";
+/*import {  Header,  Main,  Footer,  Films,  Fotografia,  Publicidad,  MotionGraphic,  DisenoGrafico,  Proyectos3d,} from "./App.js";*/
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header/Header.jsx";
+import Main from "./components/main/Main.jsx";
+import Footer from "./components/footer/Footer.jsx";
+import Films from "./pages/Films/Films.jsx";
+import Fotografia from "./pages/Fotografia/Fotografia.jsx";
+import Publicidad from "./pages/Publicidad/Publicidad.jsx";
+import MotionGraphic from "./pages/MotionGraphic/MotionGraphic.jsx";
+import DisenoGrafico from "./pages/DisenoGrafico/DisenoGrafico.jsx";
+import Proyectos3d from "./pages/Proyectos3d/Proyectos3d.jsx";
 function App() {
   return (
     <Router>
