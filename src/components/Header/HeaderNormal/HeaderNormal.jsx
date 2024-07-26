6a7197e9bc09d61f236aa3fe0d3db428aede6e19
Ajandro <adelaygleisafleitas@gmail.com>
2024-07-26 09:16:21 -0400

    9

---
+++ src/components/Header/HeaderNormal/HeaderNormal.jsx
@@ -0,0 +1,51 @@
+import {
+  handleMouseEnter,
+  handleMouseLeave,
+  handlePos,
+} from "../Tool/Tool.jsx";
+import { headerObject } from "../../data/ObjetHeader";
+import { Link } from "react-router-dom";
+import LazyLoad from "react-lazy-load";
+import Logo from "../../../assets/stuf/logos/REMACHEsudio LOGO Blanco.png";
+
+function HeaderNormal({ displayBurger, setDisplayBurger }) {
+  return (
+    <div className="container_header">
+<<<<<<< HEAD
+      <div className="container_logo">
+=======
+      <div
+        style={{
+          transform: !displayBurger ? "translateX(-33px)" : "translateX(-7px)",
+        }}
+        className="container_logo"
+      >
+>>>>>>> 1b141ebba2f39fae4908bca67da5b10c1d337fc6
+        <LazyLoad>
+          <img src={Logo} alt="logo_Remache" />
+        </LazyLoad>
+      </div>
+      <nav>
+        <div className="menu">
+          <ul>
+            {headerObject.map((item) => (
+              <li key={item.id}>
+                <Link
+                  className="encode_sans_condensed_medium"
+                  to={`${item.to}`}
+                  onClick={() => handlePos(item.type)}
+                  onMouseEnter={() => handleMouseEnter(item.type)}
+                  onMouseLeave={handleMouseLeave}
+                >
+                  {item.type}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default HeaderNormal;
