390dc6c8769403d360ea5dec08bedae026a3496b
Ajandro <adelaygleisafleitas@gmail.com>
2024-08-05 07:56:15 -0400

    13

--- src/components/Header/HeaderNormal/HeaderNormal.jsx
+++ src/components/Header/HeaderNormal/HeaderNormal.jsx
@@ -7,44 +7,35 @@ import { headerObject } from "../../data/ObjetHeader";
 import { Link } from "react-router-dom";
 import LazyLoad from "react-lazy-load";
 import Logo from "../../../assets/stuf/logos/REMACHEsudio LOGO Blanco.png";
-import LogoResponsive from "../../../assets/stuf/logos/REMACHEsudio LOGO_resposive.png";
+import LogoResponsive from "../../../assets/stuf/iconos/RE_LOGO-01.svg";
 
 function HeaderNormal({ displayBurger, setDisplayBurger }) {
   return (
     <div className="container_header">
       <div className="container_logo">
-        <div
-          style={{
-            transform: !displayBurger
-              ? "translateX(-33px)"
-              : "translateX(-7px)",
-          }}
-          className="container_logo"
-        >
-          <LazyLoad>
-            <img src={Logo} alt="logo_Remache" />
-          </LazyLoad>
-        </div>
-        <nav>
-          <div className="menu">
-            <ul>
-              {headerObject.map((item) => (
-                <li key={item.id}>
-                  <Link
-                    className="encode_sans_condensed_medium"
-                    to={`${item.to}`}
-                    onClick={() => handlePos(item.type)}
-                    onMouseEnter={() => handleMouseEnter(item.type)}
-                    onMouseLeave={handleMouseLeave}
-                  >
-                    {item.type}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </nav>
+        <LazyLoad>
+          <img src={Logo} alt="logo_Remache" />
+        </LazyLoad>
       </div>
+      <nav>
+        <div className="menu">
+          <ul>
+            {headerObject.map((item) => (
+              <li key={item.id}>
+                <Link
+                  className="encode_sans_condensed_medium"
+                  to={`${item.to}`}
+                  onClick={() => handlePos(item.type)}
+                  onMouseEnter={() => handleMouseEnter(item.type)}
+                  onMouseLeave={handleMouseLeave}
+                >
+                  {item.type}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </nav>
     </div>
   );
 }
--- src/components/data/pages/Fotografia.js
+++ src/components/data/pages/Fotografia.js
@@ -1,18 +1,59 @@
+/*imagenes galeria*/
 import bigbros from "../../../assets/stuf/pages/Fotografia/05 TRABAJOS FOTOGRAFIA copy_0000s_0000_BIG BROS.png";
 import denis from "../../../assets/stuf/pages/Fotografia/05 TRABAJOS FOTOGRAFIA copy_0000s_0001_DENYS RAMOS.png";
 import dados from "../../../assets/stuf/pages/Fotografia/05 TRABAJOS FOTOGRAFIA copy_0000s_0002_DADOR.png";
 import ana from "../../../assets/stuf/pages/Fotografia/05 TRABAJOS FOTOGRAFIA copy_0000s_0003_LA FRANJA TEATRAL.png";
 import festival from "../../../assets/stuf/pages/Fotografia/05 TRABAJOS FOTOGRAFIA copy_0000s_0004_ISLA VERDE.png";
 import saudade from "../../../assets/stuf/pages/Fotografia/05 TRABAJOS FOTOGRAFIA copy_0000s_0005_SAUDADE.png";
-
-/*imagenes galeria big bros*/
-import bbImg1 from "../../../assets/stuf/pages/Fotografia/GALERIA FOTO/bigbros/bigbros001.jpg";
-import bbImg2 from "../../../assets/stuf/pages/Fotografia/GALERIA FOTO/bigbros/bigbros002.jpg";
-import bbImg3 from "../../../assets/stuf/pages/Fotografia/GALERIA FOTO/bigbros/bigbros003.jpg";
-import bbImg4 from "../../../assets/stuf/pages/Fotografia/GALERIA FOTO/bigbros/bigbros004.jpg";
-import bbImg5 from "../../../assets/stuf/pages/Fotografia/GALERIA FOTO/bigbros/bigbros005.jpg";
-import bbImg6 from "../../../assets/stuf/pages/Fotografia/GALERIA FOTO/bigbros/bigbros006.jpg";
 /*imagenes galeria*/
+/*imagenes galeria big bros*/
+import bbImg1 from "../../../assets/stuf/pages/Fotografia/GALERIA FOTO/GALERIA FOTO 01/BIG BROS (1).jpg";
+import bbImg2 from "../../../assets/stuf/pages/Fotografia/GALERIA FOTO/GALERIA FOTO 01/BIG BROS (2).jpg";
+import bbImg3 from "../../../assets/stuf/pages/Fotografia/GALERIA FOTO/GALERIA FOTO 01/BIG BROS (3).jpg";
+import bbImg4 from "../../../assets/stuf/pages/Fotografia/GALERIA FOTO/GALERIA FOTO 01/BIG BROS (4).jpg";
+import bbImg5 from "../../../assets/stuf/pages/Fotografia/GALERIA FOTO/GALERIA FOTO 01/BIG BROS (5).jpg";
+import bbImg6 from "../../../assets/stuf/pages/Fotografia/GALERIA FOTO/GALERIA FOTO 01/BIG BROS (6).jpg";
+/*imagenes galeria big bros*/
+/*imagenes galeria denys*/
+import denys1 from "../../../assets/stuf/pages/Fotografia/GALERIA FOTO/GALERIA FOTO 02/DENYS RAMOS (1).jpg";
+import denys2 from "../../../assets/stuf/pages/Fotografia/GALERIA FOTO/GALERIA FOTO 02/DENYS RAMOS (2).jpg";
+import denys3 from "../../../assets/stuf/pages/Fotografia/GALERIA FOTO/GALERIA FOTO 02/DENYS RAMOS (3).jpg";
+import denys4 from "../../../assets/stuf/pages/Fotografia/GALERIA FOTO/GALERIA FOTO 02/DENYS RAMOS (4).jpg";
+import denys5 from "../../../assets/stuf/pages/Fotografia/GALERIA FOTO/GALERIA FOTO 02/DENYS RAMOS (5).jpg";
+import denys6 from "../../../assets/stuf/pages/Fotografia/GALERIA FOTO/GALERIA FOTO 02/DENYS RAMOS (6).jpg";
+/*imagenes galeria denys*/
+/*imagenes galeria dador*/
+import dador1 from "../../../assets/stuf/pages/Fotografia/GALERIA FOTO/GALERIA FOTO 03/DADOR (1).jpg";
+import dador2 from "../../../assets/stuf/pages/Fotografia/GALERIA FOTO/GALERIA FOTO 03/DADOR (2).jpg";
+import dador3 from "../../../assets/stuf/pages/Fotografia/GALERIA FOTO/GALERIA FOTO 03/DADOR (3).jpg";
+import dador4 from "../../../assets/stuf/pages/Fotografia/GALERIA FOTO/GALERIA FOTO 03/DADOR (4).jpg";
+import dador5 from "../../../assets/stuf/pages/Fotografia/GALERIA FOTO/GALERIA FOTO 03/DADOR (5).jpg";
+import dador6 from "../../../assets/stuf/pages/Fotografia/GALERIA FOTO/GALERIA FOTO 03/DADOR (6).jpg";
+/*imagenes galeria dador*/
+/*imagenes galeria ana*/
+import ana1 from "../../../assets/stuf/pages/Fotografia/GALERIA FOTO/GALERIA FOTO 04/ANA (1).jpg";
+import ana2 from "../../../assets/stuf/pages/Fotografia/GALERIA FOTO/GALERIA FOTO 04/ANA (2).jpg";
+import ana3 from "../../../assets/stuf/pages/Fotografia/GALERIA FOTO/GALERIA FOTO 04/ANA (3).jpg";
+import ana4 from "../../../assets/stuf/pages/Fotografia/GALERIA FOTO/GALERIA FOTO 04/ANA (4).jpg";
+import ana5 from "../../../assets/stuf/pages/Fotografia/GALERIA FOTO/GALERIA FOTO 04/ANA (5).jpg";
+import ana6 from "../../../assets/stuf/pages/Fotografia/GALERIA FOTO/GALERIA FOTO 04/ANA (6).jpg";
+/*imagenes galeria ana*/
+/*imagenes galeria ISLA VERDE*/
+import iv1 from "../../../assets/stuf/pages/Fotografia/GALERIA FOTO/GALERIA FOTO 05/ISLA VERDE (1).jpg";
+import iv2 from "../../../assets/stuf/pages/Fotografia/GALERIA FOTO/GALERIA FOTO 05/ISLA VERDE (2).jpg";
+import iv3 from "../../../assets/stuf/pages/Fotografia/GALERIA FOTO/GALERIA FOTO 05/ISLA VERDE (3).jpg";
+import iv4 from "../../../assets/stuf/pages/Fotografia/GALERIA FOTO/GALERIA FOTO 05/ISLA VERDE (4).jpg";
+import iv5 from "../../../assets/stuf/pages/Fotografia/GALERIA FOTO/GALERIA FOTO 05/ISLA VERDE (5).jpg";
+import iv6 from "../../../assets/stuf/pages/Fotografia/GALERIA FOTO/GALERIA FOTO 05/ISLA VERDE (6).jpg";
+/*imagenes galeria ISLA VERDE*/
+/*imagenes galeria saudade*/
+import saudade1 from "../../../assets/stuf/pages/Fotografia/GALERIA FOTO/GALERIA FOTO 06/SAUDADE (1).jpg";
+import saudade2 from "../../../assets/stuf/pages/Fotografia/GALERIA FOTO/GALERIA FOTO 06/SAUDADE (2).jpg";
+import saudade3 from "../../../assets/stuf/pages/Fotografia/GALERIA FOTO/GALERIA FOTO 06/SAUDADE (3).jpg";
+import saudade4 from "../../../assets/stuf/pages/Fotografia/GALERIA FOTO/GALERIA FOTO 06/SAUDADE (4).jpg";
+import saudade5 from "../../../assets/stuf/pages/Fotografia/GALERIA FOTO/GALERIA FOTO 06/SAUDADE (5).jpg";
+import saudade6 from "../../../assets/stuf/pages/Fotografia/GALERIA FOTO/GALERIA FOTO 06/SAUDADE (6).jpg";
+/*imagenes galeria saudade*/
 
 export let fotografia = [
   {
@@ -27,7 +68,7 @@ export let fotografia = [
     name: "Denys Ramos",
     text: "Fotos de estudio para el dossier personal del actor ",
     url: "https://youtu.be/kV90SSCeBvs",
-    imgArray: [bbImg1, bbImg2, bbImg3, bbImg4, bbImg5, bbImg6],
+    imgArray: [denys1, denys2, denys3, denys4, denys5, denys6],
     image: denis,
     id: 2,
   },
@@ -35,7 +76,7 @@ export let fotografia = [
     name: "Dador 2024",
     text: "Fotos en exteriores para la marca de ropa Dador y su colección verano de 2024",
     url: "https://youtu.be/kV90SSCeBvs",
-    imgArray: [bbImg1, bbImg2, bbImg3, bbImg4, bbImg5, bbImg6],
+    imgArray: [dador1, dador2, dador3, dador4, dador5, dador6],
     image: dados,
     id: 3,
   },
@@ -43,7 +84,7 @@ export let fotografia = [
     name: "ANA, LA GENTE ESTA MIRANDO LA SANGRE",
     text: "Fotos para la puesta en escena de La franja teatral dirigida por Agnieska Hernández ",
     url: "https://youtu.be/kV90SSCeBvs",
-    imgArray: [bbImg1, bbImg2, bbImg3, bbImg4, bbImg5, bbImg6],
+    imgArray: [ana1, ana2, ana3, ana4, ana5, ana6],
     image: ana,
     id: 4,
   },
@@ -51,7 +92,7 @@ export let fotografia = [
     name: "Festival isla verde",
     text: "Fotos en clausura del festival dirigido por Jorge Perugorria donde se presento Silvio Rodríguez",
     url: "https://youtu.be/kV90SSCeBvs",
-    imgArray: [bbImg1, bbImg2, bbImg3, bbImg4, bbImg5, bbImg6],
+    imgArray: [iv1, iv2, iv3, iv4, iv5, iv6],
     image: festival,
     id: 5,
   },
@@ -59,7 +100,7 @@ export let fotografia = [
     name: "Saudade",
     text: "Fotos para obra de danza de la compañia Persona dirigida por Sandra Rami",
     url: "https://youtu.be/kV90SSCeBvs",
-    imgArray: [bbImg1, bbImg2, bbImg3, bbImg4, bbImg5, bbImg6],
+    imgArray: [saudade1, saudade2, saudade3, saudade4, saudade5, saudade6],
     image: saudade,
     id: 6,
   },
