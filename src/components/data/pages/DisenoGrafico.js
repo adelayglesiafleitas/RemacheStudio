4255dc0b5386a86328418da486195d9b642e4857
Ajandro <adelaygleisafleitas@gmail.com>
2024-08-07 21:53:56 -0400

    18

--- src/components/data/pages/DisenoGrafico.js
+++ src/components/data/pages/DisenoGrafico.js
@@ -5,14 +5,74 @@ import seySer from "../../../assets/stuf/pages/DisenoGrafico/06 TRABAJOS DISEÑO
 import lucas from "../../../assets/stuf/pages/DisenoGrafico/bailando con margot.png06 TRABAJOS DISEÑO GRAFICO copy_0000s_0004_GRAFICA 05.png";
 import danza from "../../../assets/stuf/pages/DisenoGrafico/06 TRABAJOS DISEÑO GRAFICO copy_0000s_0005_GRAFICA 06.png";
 
-/*imagenes galeria*/
-import bbImg1 from "../../../assets/stuf/pages/Fotografia/GALERIA FOTO/GALERIA FOTO 01/BIG BROS (1).jpg";
-import bbImg2 from "../../../assets/stuf/pages/Fotografia/GALERIA FOTO/GALERIA FOTO 01/BIG BROS (2).jpg";
-import bbImg3 from "../../../assets/stuf/pages/Fotografia/GALERIA FOTO/GALERIA FOTO 01/BIG BROS (3).jpg";
-import bbImg4 from "../../../assets/stuf/pages/Fotografia/GALERIA FOTO/GALERIA FOTO 01/BIG BROS (4).jpg";
-import bbImg5 from "../../../assets/stuf/pages/Fotografia/GALERIA FOTO/GALERIA FOTO 01/BIG BROS (5).jpg";
-import bbImg6 from "../../../assets/stuf/pages/Fotografia/GALERIA FOTO/GALERIA FOTO 01/BIG BROS (6).jpg";
-/*imagenes galeria*/
+/*imagenes carteles*/
+import nft from "../../../assets/stuf/pages/DisenoGrafico/CARTELES/CARTEL NFT.jpg";
+import ceri from "../../../assets/stuf/pages/DisenoGrafico/CARTELES/CERIGRAFICO.jpg";
+import demon from "../../../assets/stuf/pages/DisenoGrafico/CARTELES/DEMONIOS_EN_LA_ALMOHADA.jpg";
+import expo from "../../../assets/stuf/pages/DisenoGrafico/CARTELES/EXPO.jpg";
+import lucasc from "../../../assets/stuf/pages/DisenoGrafico/CARTELES/LUCAS.jpg";
+import poster from "../../../assets/stuf/pages/DisenoGrafico/CARTELES/POSTER 33 CLFF.jpg";
+import rudy from "../../../assets/stuf/pages/DisenoGrafico/CARTELES/RUDY.jpg";
+import wall from "../../../assets/stuf/pages/DisenoGrafico/CARTELES/THE WALL.jpg";
+import vill from "../../../assets/stuf/pages/DisenoGrafico/CARTELES/VILLANOS.jpg";
+/*imagenes carteles*/
+/*imagenes BALLET*/
+import dance from "../../../assets/stuf/pages/DisenoGrafico/GRAFICA BALLET/DANCE OPEN LOGO.jpg";
+import contacto from "../../../assets/stuf/pages/DisenoGrafico/GRAFICA BALLET/FOTO CONTACTO .jpg";
+import perfil from "../../../assets/stuf/pages/DisenoGrafico/GRAFICA BALLET/FOTO DE PERFIL.jpg";
+import pass from "../../../assets/stuf/pages/DisenoGrafico/GRAFICA BALLET/GOLDEN PASS 2025 .jpg";
+import placas from "../../../assets/stuf/pages/DisenoGrafico/GRAFICA BALLET/PLACAS ESPAÑA_00007.jpg";
+import poster_1 from "../../../assets/stuf/pages/DisenoGrafico/GRAFICA BALLET/POSTER 07 .jpg";
+import poster_2 from "../../../assets/stuf/pages/DisenoGrafico/GRAFICA BALLET/POSTER 08 .jpg";
+import taiwan from "../../../assets/stuf/pages/DisenoGrafico/GRAFICA BALLET/TAIWAN_00016.jpg";
+/*imagenes BALLET*/
+/*imagenes LUCAS*/
+import graco_baca from "../../../assets/stuf/pages/DisenoGrafico/GRAFICA LUCAS/DRAGON BACALLAO 1.jpg";
+import graco_pau from "../../../assets/stuf/pages/DisenoGrafico/GRAFICA LUCAS/DRAGON PAULA 1.jpg";
+import espada_baca from "../../../assets/stuf/pages/DisenoGrafico/GRAFICA LUCAS/ESPADA BACALLAO.jpg";
+import invitacion from "../../../assets/stuf/pages/DisenoGrafico/GRAFICA LUCAS/INVITACION.jpg";
+import ojos_pau from "../../../assets/stuf/pages/DisenoGrafico/GRAFICA LUCAS/OJOS PAULA.jpg";
+import ojos_sil from "../../../assets/stuf/pages/DisenoGrafico/GRAFICA LUCAS/OJOS SILVA.jpg";
+import trono_pau from "../../../assets/stuf/pages/DisenoGrafico/GRAFICA LUCAS/TRONO PAULA.jpg";
+/*imagenes LUCAS*/
+/*imagenes GRAFICA LUZ ROJA*/
+import roja_1 from "../../../assets/stuf/pages/DisenoGrafico/GRAFICA LUZ ROJA/001.jpg";
+import roja_2 from "../../../assets/stuf/pages/DisenoGrafico/GRAFICA LUZ ROJA/002.jpg";
+import roja_3 from "../../../assets/stuf/pages/DisenoGrafico/GRAFICA LUZ ROJA/003.jpg";
+import roja_4 from "../../../assets/stuf/pages/DisenoGrafico/GRAFICA LUZ ROJA/004.jpg";
+import roja_5 from "../../../assets/stuf/pages/DisenoGrafico/GRAFICA LUZ ROJA/005.jpg";
+import roja_6 from "../../../assets/stuf/pages/DisenoGrafico/GRAFICA LUZ ROJA/006.jpg";
+import roja_7 from "../../../assets/stuf/pages/DisenoGrafico/GRAFICA LUZ ROJA/007.jpg";
+import roja_8 from "../../../assets/stuf/pages/DisenoGrafico/GRAFICA LUZ ROJA/008.jpg";
+import roja_9 from "../../../assets/stuf/pages/DisenoGrafico/GRAFICA LUZ ROJA/009.jpg";
+/*imagenes GRAFICA LUZ ROJA*/
+/*imagenes GRAFICA GRAFICA SEYCEL*/
+import portada from "../../../assets/stuf/pages/DisenoGrafico/GRAFICA SEYCEL/PORTADA.jpg";
+import reverso from "../../../assets/stuf/pages/DisenoGrafico/GRAFICA SEYCEL/REVERSO DE CONTRA (2).jpg";
+import sobre from "../../../assets/stuf/pages/DisenoGrafico/GRAFICA SEYCEL/SOBRE.jpg";
+/*imagenes GRAFICA GRAFICA SEYCEL*/
+/*imagenes GRAFICA GRAFICA logos*/
+import logo_1 from "../../../assets/stuf/pages/DisenoGrafico/LOGOS GALERIA/LOGO (1).jpg";
+import logo_2 from "../../../assets/stuf/pages/DisenoGrafico/LOGOS GALERIA/LOGO (2).jpg";
+import logo_3 from "../../../assets/stuf/pages/DisenoGrafico/LOGOS GALERIA/LOGO (3).jpg";
+import logo_4 from "../../../assets/stuf/pages/DisenoGrafico/LOGOS GALERIA/LOGO (4).jpg";
+import logo_5 from "../../../assets/stuf/pages/DisenoGrafico/LOGOS GALERIA/LOGO (5).jpg";
+import logo_6 from "../../../assets/stuf/pages/DisenoGrafico/LOGOS GALERIA/LOGO (6).jpg";
+import logo_7 from "../../../assets/stuf/pages/DisenoGrafico/LOGOS GALERIA/LOGO (7).jpg";
+import logo_8 from "../../../assets/stuf/pages/DisenoGrafico/LOGOS GALERIA/LOGO (8).jpg";
+import logo_9 from "../../../assets/stuf/pages/DisenoGrafico/LOGOS GALERIA/LOGO (9).jpg";
+import logo_10 from "../../../assets/stuf/pages/DisenoGrafico/LOGOS GALERIA/LOGO (10).jpg";
+import logo_11 from "../../../assets/stuf/pages/DisenoGrafico/LOGOS GALERIA/LOGO (11).jpg";
+import logo_12 from "../../../assets/stuf/pages/DisenoGrafico/LOGOS GALERIA/LOGO (12).jpg";
+import logo_13 from "../../../assets/stuf/pages/DisenoGrafico/LOGOS GALERIA/LOGO (13).jpg";
+import logo_14 from "../../../assets/stuf/pages/DisenoGrafico/LOGOS GALERIA/LOGO (14).jpg";
+import logo_15 from "../../../assets/stuf/pages/DisenoGrafico/LOGOS GALERIA/LOGO (15).jpg";
+import logo_16 from "../../../assets/stuf/pages/DisenoGrafico/LOGOS GALERIA/LOGO (16).jpg";
+import logo_17 from "../../../assets/stuf/pages/DisenoGrafico/LOGOS GALERIA/LOGO (17).jpg";
+import logo_18 from "../../../assets/stuf/pages/DisenoGrafico/LOGOS GALERIA/LOGO (18).jpg";
+import logo_19 from "../../../assets/stuf/pages/DisenoGrafico/LOGOS GALERIA/LOGO (19).jpg";
+import logo_20 from "../../../assets/stuf/pages/DisenoGrafico/LOGOS GALERIA/LOGO (20).jpg";
+/*imagenes GRAFICA GRAFICA logos*/
 
 export let disenoGrafico = [
   {
@@ -20,7 +80,28 @@ export let disenoGrafico = [
     text: "Recopilación de algunos de nuestros diseños de logos",
     url: "https://youtu.be/kV90SSCeBvs",
     image: clave,
-    imgArray: [bbImg1, bbImg2, bbImg3, bbImg4, bbImg5, bbImg6],
+    imgArray: [
+      logo_1,
+      logo_2,
+      logo_3,
+      logo_4,
+      logo_5,
+      logo_6,
+      logo_7,
+      logo_8,
+      logo_9,
+      logo_10,
+      logo_11,
+      logo_12,
+      logo_13,
+      logo_14,
+      logo_15,
+      logo_16,
+      logo_17,
+      logo_18,
+      logo_19,
+      logo_20,
+    ],
     id: 1,
   },
   {
@@ -28,7 +109,7 @@ export let disenoGrafico = [
     text: "Muestra de algunos carteles diseñados por nosotros",
     url: "https://youtu.be/kV90SSCeBvs",
     image: whats,
-    imgArray: [bbImg1, bbImg2, bbImg3, bbImg4, bbImg5, bbImg6],
+    imgArray: [nft, ceri, demon, expo, lucasc, poster, rudy, wall, vill],
     id: 2,
   },
   {
@@ -36,7 +117,17 @@ export let disenoGrafico = [
     text: "Diseño de Catalogo para expo de William Acosta",
     url: "https://youtu.be/kV90SSCeBvs",
     image: luzRoja,
-    imgArray: [bbImg1, bbImg2, bbImg3, bbImg4, bbImg5, bbImg6],
+    imgArray: [
+      roja_1,
+      roja_2,
+      roja_3,
+      roja_4,
+      roja_5,
+      roja_6,
+      roja_7,
+      roja_8,
+      roja_9,
+    ],
     id: 3,
   },
   {
@@ -44,7 +135,7 @@ export let disenoGrafico = [
     text: "Spot para espectaculo musical con Lia Rodríguez realizado por Remachestudio junto a Alejandro Menendez",
     url: "https://youtu.be/kV90SSCeBvs",
     image: seySer,
-    imgArray: [bbImg1, bbImg2, bbImg3, bbImg4, bbImg5, bbImg6],
+    imgArray: [portada, reverso, sobre],
     id: 4,
   },
   {
@@ -52,7 +143,15 @@ export let disenoGrafico = [
     text: "Diseño de grafica para la campaña del premio",
     url: "https://youtu.be/kV90SSCeBvs",
     image: lucas,
-    imgArray: [bbImg1, bbImg2, bbImg3, bbImg4, bbImg5, bbImg6],
+    imgArray: [
+      graco_baca,
+      graco_pau,
+      espada_baca,
+      invitacion,
+      ojos_pau,
+      ojos_sil,
+      trono_pau,
+    ],
     id: 5,
   },
   {
@@ -60,7 +159,16 @@ export let disenoGrafico = [
     text: "Diseño de identidad para el gran premio de ballet",
     url: "https://youtu.be/kV90SSCeBvs",
     image: danza,
-    imgArray: [bbImg1, bbImg2, bbImg3, bbImg4, bbImg5, bbImg6],
+    imgArray: [
+      dance,
+      perfil,
+      contacto,
+      pass,
+      placas,
+      poster_1,
+      poster_2,
+      taiwan,
+    ],
     id: 6,
   },
 ];
